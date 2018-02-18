14f9d42834633708e03bb2806436b6c97300bbe7
Michael Kochell <michael@iris-works.com>
2018-02-18 18:56:25 -0500

    move repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="top-spacing" />
+        <h1>Mr. Smarty Britches</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
